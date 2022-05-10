19bfbc02faa32fcd6bf1318c7dfaf8444101d561
phpmasta <60963120+Maxkucera7@users.noreply.github.com>
2022-05-10 19:20:17 +0200

    Add files via upload

---
+++ Hra 15/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hra_15
+{
+    public partial class Form1 : Form
+    {
+        Segment[,] segments = new Segment[3, 3];
+        Random rand = new Random();
+
+        int size = 3;
+
+        public Form1() {
+            InitializeComponent();
+
+        }
+
+    }
+}
---
+++ Hra 15/Segment.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hra_15
+{
+    public partial class Segment : UserControl
+    {
+        public event Action<Segment> PositionChangeRequested;
+
+        int cislo;
+
+        public int Cislo {
+            get {
+                return cislo;
+            }
+        }
+        public int IndexX {
+            get {
+                return this.Location.X / this.Width;
+            }
+        }
+        public int IndexY {
+            get {
+                return this.Location.Y / this.Height;
+            }
+        }
+        public Segment(int a):this() {
+            cislo = a;
+            label1.Text = cislo.ToString();
+
+
+        }
+
+
+        public Segment() {
+
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+            PositionChangeRequested?.Invoke(this);
+        }
+    }
+}
